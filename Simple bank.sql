1a674364b7a07e197558cd3db9ef2759a0466653
shobander <personal65-one@yahoo.com>
2021-08-23 22:45:05 +0200

    Database design

---
+++ Simple bank.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz DEFAULT 'now()'
+);
+
+CREATE TABLE "enteries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint,
+  "to_account_id" bigint,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+ALTER TABLE "enteries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "enteries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "enteries"."amount" IS 'can be negative or positive';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
